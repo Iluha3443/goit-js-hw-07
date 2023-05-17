a87dac1550d7ca3809d1a5347fb634b151d55a27
Iluha3443 <tkacenkoila01@gmail.com>
2023-05-17 20:53:29 +0300

    task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
  
 const container = document.querySelector('.gallery');
 const markup = galleryItems.map(({ preview, original, description }) =>
-    `<li> <img src="${preview}" alt="${description}" class ="gallery__image" /></li>`);
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="Image description"
+    />
+  </a>
+</li>`);
 
 container.insertAdjacentHTML('beforeend', markup.join(""));
 container.addEventListener('click', onClick);
 
+
+
+
+
 function onClick(event) {
-    console.log(event.target)
+    event.preventDefault();
+    const target = event.target;
+    if (!target.classList.contains('gallery__link')) { 
+        return;
+    }
+      const instance = basicLightbox.create(`
+      <img src="${target.dataset.source}" width="800" height="600"> `
+   );
+   instance.show()
 
-}
+        
+    document.addEventListener("keydown", (evt) => {
+        if (evt.code === "Escape") {
+            instance.close()
+        }
 
-       
-      
-      
-// preview:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-//     description: 'Hokkaido Flower',
+    })   
+};
+
+
+
+    
+     
+ 
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+const markupGallery = galleryItems.map(({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="Image description" />
+   </a>
+</li>`);
+
+container.insertAdjacentHTML('beforeend', markupGallery.join(""));
